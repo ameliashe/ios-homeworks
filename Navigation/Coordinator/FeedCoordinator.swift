efca2958a521b83cfe2e6e8bc203acac09b05c3b
ameliashe <ameliashe@icloud.com>
2025-01-11 07:19:45 +0400

    HW8 complete. Added default text for login and password textfields on LoginVC.

---
+++ Navigation/Coordinator/FeedCoordinator.swift
@@ -0,0 +1,28 @@
+//
+//  Coordinator.swift
+//  Navigation
+//
+//  Created by Amelia Romanova on 1/10/25.
+//
+
+import Foundation
+import UIKit
+
+class FeedCoordinator: Coordinator {
+
+	var navigationController: UINavigationController
+
+
+	init(navigationController: UINavigationController) {
+		self.navigationController = navigationController
+	}
+
+	func start() {
+		let feedModel = FeedModel()
+		let feedViewModel = FeedViewModel(model: feedModel)
+		let feedVC = FeedViewController(viewModel: feedViewModel)
+
+		navigationController.pushViewController(feedVC, animated: false)
+	}
+
+}
---
+++ Navigation/Coordinator/MainCoordinator.swift
@@ -0,0 +1,54 @@
+//
+//  Coordinator.swift
+//  Navigation
+//
+//  Created by Amelia Romanova on 1/10/25.
+//
+
+import Foundation
+import UIKit
+
+protocol Coordinator {
+	var navigationController: UINavigationController { get set }
+	func start()
+}
+
+class MainCoordinator: Coordinator {
+
+	var navigationController = UINavigationController()
+	var tabBarController = UITabBarController()
+
+	var feedCoordinator: FeedCoordinator?
+	var profileCoordinator: ProfileCoordinator?
+
+	init(navigationController: UINavigationController, tabBarController: UITabBarController) {
+		self.navigationController = navigationController
+		self.tabBarController = tabBarController
+	}
+
+	func start() {
+
+		let feedNC = UINavigationController()
+		let profileNC = UINavigationController()
+
+		feedCoordinator = FeedCoordinator(navigationController:	feedNC)
+		profileCoordinator = ProfileCoordinator(navigationController: profileNC)
+
+		feedCoordinator?.start()
+		profileCoordinator?.start()
+
+//		guard let feedNC, let profileNC else { return }
+
+		let controllers = [feedNC, profileNC]
+		tabBarController.viewControllers = controllers
+
+		feedNC.tabBarItem = UITabBarItem(title: "Feed", image: UIImage(systemName: "list.bullet.rectangle"), tag: 0)
+		profileNC.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(systemName: "person"), tag: 1)
+
+		tabBarController.selectedIndex = 0
+		tabBarController.tabBar.isTranslucent = false
+
+		navigationController.viewControllers = [tabBarController]
+	}
+
+}
